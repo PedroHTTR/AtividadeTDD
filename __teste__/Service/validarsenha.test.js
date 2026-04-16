f64f60b8e8e9a971c9d809bbed34b4ee8533c2c8
Pedro <pedrohtr661@gmail.com>
2026-04-16 20:07:47 -0300

    Criação de teste para validar que as senhas possuam ao menos um númeral, um caractere especial e não possuam espaços em branco.

--- __teste__/Service/validarsenha.test.js
+++ __teste__/Service/validarsenha.test.js
@@ -24,3 +24,30 @@ test('Verificar se a senha possui pelo menos uma letra minuscula', () => {
     // Assert
     expect(resultado).toBe(false);
 });
+
+test('Verificar se a senha possui pelo menos um número', () => {
+    // Arrange
+    const senha = 'abcdefghA'; 
+    // Act
+    const resultado = validarSenha(senha);
+    // Assert
+    expect(resultado).toBe(false);
+});
+
+test('Verificar se a senha possui pelo menos um caractere especial', () => {
+    // Arrange
+    const senha = 'Abcdefg1';
+    // Act
+    const resultado = validarSenha(senha);      
+    // Assert
+    expect(resultado).toBe(false);
+});
+
+test('Garantir que a senha não possa conter espaços em branco', () => {
+    // Arrange
+    const senha = 'Abc defg1!';
+    // Act
+    const resultado = validarSenha(senha);
+    // Assert
+    expect(resultado).toBe(false);
+});
